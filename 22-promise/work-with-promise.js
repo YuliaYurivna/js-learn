8b54a6cf3bdb382b6400def0ff905a7f341b2cd9
YuliaYurivna <yuliachumak97@gmail.com>
2022-05-09 18:15:38 +0300

    Practice with promise.

--- 22-promise/work-with-promise.js
+++ 22-promise/work-with-promise.js
@@ -1,1 +1,16 @@
-const myPromise = new Promise();
+// const myPromise = new Promise((resolve, reject) => {
+
+// });
+
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+// .then(response => {
+//    console.log(response)
+//    return response.json() 
+// })
+// .then(json => console.log(json))
+// .catch(error => console.error(error))
+
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+.then(response => response.json())
+.then(json => console.log(json))
+.catch(error => console.error(error))
